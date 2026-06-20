34fec2137d74122cf038047bfa78d4b0a962e268
Suraj-singh04 <surajsingh877028@gmail.com>
2026-06-20 23:52:43 +0530

    Implemented Factory Method Pattern

---
+++ 01-DesignPatterns/FactoryMethodPatternExample/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        DocumentFactory factory;
+
+        factory = new WordFactory();
+        factory = new PdfFactory();
+
+        Document document = factory.createDocument();
+
+        document.open();
+    }
+}
