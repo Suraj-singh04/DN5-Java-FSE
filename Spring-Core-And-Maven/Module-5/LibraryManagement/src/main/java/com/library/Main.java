43e468ed8a96464a7afdd1f2d4ea9ebfcb0e1705
Suraj-singh04 <surajsingh877028@gmail.com>
2026-07-12 15:31:09 +0530

    feat(module-5): configure Spring IoC container and bean management

--- Spring-Core-And-Maven/Module-5/LibraryManagement/src/main/java/com/library/Main.java
+++ Spring-Core-And-Maven/Module-5/LibraryManagement/src/main/java/com/library/Main.java
@@ -4,11 +4,29 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        BookService service = context.getBean("bookService", BookService.class);
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        BookRepository repository =
+                context.getBean("bookRepository", BookRepository.class);
+
         service.display();
+
+        BookRepository repo1 =
+        context.getBean("bookRepository", BookRepository.class);
+
+        BookRepository repo2 =
+                context.getBean("bookRepository", BookRepository.class);
+
+        System.out.println(repo1 == repo2);
+
+        System.out.println(repository.getBook());
+
     }
 }
