003d1fddb237a1fcc394d8c780674c529e828bc5
Suraj-singh04 <surajsingh877028@gmail.com>
2026-07-17 02:46:24 +0530

    feat(module-7): implement REST service to return country list

---  Products-and-Frameworks/Module-7/Spring-REST-Using-Spring-Boot-3/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++  Products-and-Frameworks/Module-7/Spring-REST-Using-Spring-Boot-3/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -1,16 +1,25 @@
 package com.cognizant.springlearn.controller;
 
 import com.cognizant.springlearn.model.Country;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cognizant.springlearn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CountryController {
 
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/country")
     public Country getCountry() {
+        return new Country("IN", "India");
+    }
 
-        return new Country("IN","India");
-
+    @GetMapping("/countries")
+    public List<Country> getCountries() {
+        return countryService.getAllCountries();
     }
 }
---
+++  Products-and-Frameworks/Module-7/Spring-REST-Using-Spring-Boot-3/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -0,0 +1,23 @@
+package com.cognizant.springlearn.service;
+
+import com.cognizant.springlearn.model.Country;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    public List<Country> getAllCountries() {
+
+        List<Country> countries = new ArrayList<>();
+
+        countries.add(new Country("IN", "India"));
+        countries.add(new Country("US", "United States"));
+        countries.add(new Country("JP", "Japan"));
+        countries.add(new Country("DE", "Germany"));
+
+        return countries;
+    }
+}
