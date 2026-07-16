7985f38e83a2c78b49c3bcf0f8353d6fb6d83399
Suraj-singh04 <surajsingh877028@gmail.com>
2026-07-17 02:39:16 +0530

    feat(module-7): implement REST endpoint to return country details using jackson under the hood.

---
+++  Products-and-Frameworks/Module-7/Spring-REST-Using-Spring-Boot-3/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,16 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.model.Country;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/country")
+    public Country getCountry() {
+
+        return new Country("IN","India");
+
+    }
+}
