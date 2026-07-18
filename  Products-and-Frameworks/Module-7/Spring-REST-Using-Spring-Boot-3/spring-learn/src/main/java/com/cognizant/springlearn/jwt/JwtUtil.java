2e77e523b05c7a2b2694dfd3774ae8464fd931ce
Suraj-singh04 <surajsingh877028@gmail.com>
2026-07-18 23:21:22 +0530

    feat(module-7): implement JWT utility for token generation and validation

---
+++  Products-and-Frameworks/Module-7/Spring-REST-Using-Spring-Boot-3/spring-learn/src/main/java/com/cognizant/springlearn/jwt/JwtUtil.java
@@ -0,0 +1,53 @@
+package com.cognizant.springlearn.jwt;
+
+import java.util.Date;
+
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.nio.charset.StandardCharsets;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JwtUtil {
+    private static final String SECRET =
+        "mysecretkeymysecretkeymysecretkey123456";
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+    }
+}
